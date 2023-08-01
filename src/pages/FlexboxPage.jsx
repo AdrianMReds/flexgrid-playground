a4330b7c180f5ca3877ad17549abae941d28b57c
Adrián Montemayor Rojas <adrianmontemayor.r@gmail.com>
2023-07-31 18:05:32 -0600

    Added navbar and initial flex playground

--- src/pages/FlexboxPage.jsx
+++ src/pages/FlexboxPage.jsx
@@ -1,7 +1,19 @@
 import React from "react";
+import "../styles/FlexboxPage.css";
 
 const FlexboxPage = () => {
-  return <div>FlexboxPage</div>;
+  return (
+    <div className="flexbox-page">
+      <div className="half">
+        <h1>Container</h1>
+        <div className="container playground"></div>
+      </div>
+      <div className="half">
+        <h1>Settings</h1>
+        <div className="container"></div>
+      </div>
+    </div>
+  );
 };
 
 export default FlexboxPage;
